4470aad90e54303262727be1e75f4d924b76c130
Denis <xsekretarx@gmail.com>
2025-11-21 19:42:58 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import ‘./globals.css’
+import './globals.css'
 
 export const metadata = {
-title: ‘Denis Mikhin - Photographer’,
-description: ‘Photography portfolio by Denis Mikhin’,
+  title: 'Denis Mikhin - Photographer',
+  description: 'Photography portfolio by Denis Mikhin',
 }
 
 export default function RootLayout({ children }) {
-return (
-<html lang="ru">
-<body>{children}</body>
-</html>
-)
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
 }
